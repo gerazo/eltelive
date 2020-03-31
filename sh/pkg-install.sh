0d203671be281850efceb10bac38f9390ba0c9da
Zoltan Gera <gerazo@caesar.elte.hu>
2020-03-31 15:43:36 +0200

    Launching all needed services

--- sh/pkg-install.sh
+++ sh/pkg-install.sh
@@ -2,14 +2,16 @@
 
 echo "Installing service dependencies..."
 
-ALPINE_PKGS="nginx nginx-mod-rtmp fcgiwrap ffmpeg"
-DEBIAN_PKGS="nginx-light libnginx-mod-rtmp libnginx-mod-http-auth-pam fcgiwrap ffmpeg"
+ALPINE_PKGS="nginx nginx-mod-rtmp fcgiwrap spawn-fcgi ffmpeg"
+DEBIAN_PKGS="nginx-light libnginx-mod-rtmp libnginx-mod-http-auth-pam fcgiwrap spawn-fcgi ffmpeg"
 
 case "$EL_OS" in
   "alpine")
     apk update
     apk upgrade
     apk add $ALPINE_PKGS
+    printf "#!/bin/sh\n/usr/sbin/crond" >/usr/sbin/cron && chmod +x /usr/sbin/cron
+    ln -s /usr/bin/fcgiwrap /usr/sbin/fcgiwrap
     ;;
   "debian")
     DEBIAN_FRONTEND=noninteractive apt-get -qy update
