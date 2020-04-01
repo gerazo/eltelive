76c7f9d1a4782aa3fc4c50a110defbbcf672ddc8
Zoltan Gera <gerazo@caesar.elte.hu>
2020-04-01 21:16:20 +0200

    Important services are running in container, config is still needed

--- clean-project-n-data.sh
+++ clean-project-n-data.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 echo "Cleaning up everything in the project..."
+if [ -f config ] &&. ./config ; then
+  docker stop $EL_CONTAINERNAME
+  docker rm $EL_CONTAINERNAME
+  docker rmi $EL_CONTAINERNAME
+fi
 rm -rf config deploy
--- eltelive.sh
+++ eltelive.sh
@@ -3,9 +3,7 @@
 EL_CONFIG="config"
 EL_DEPLOY="deploy"
 EL_CONTROL="control"
-EL_RECORDING="recording"
-EL_VIDEOS="videos"
-EL_WWW="www"
+EL_DATA="data"
 EL_LOG="log"
 
 echo "ELTELive Control Script"
@@ -35,9 +33,7 @@ if [ "$EL_CONTAINER" = "docker" ] && [ "$( command -v docker )" = "" ]; then
 fi
 
 mkdir -p $EL_DEPLOY/$EL_CONTROL
-mkdir -p $EL_DEPLOY/$EL_RECORDING
-mkdir -p $EL_DEPLOY/$EL_VIDEOS
-mkdir -p $EL_DEPLOY/$EL_WWW
+mkdir -p $EL_DEPLOY/$EL_DATA
 mkdir -p $EL_DEPLOY/$EL_LOG
 
 cp sh/* $EL_DEPLOY/$EL_CONTROL/
@@ -55,24 +51,46 @@ case "$EL_CONTAINER" in
         ;;
     esac
     cat ../../tmpl/Dockerfile | sed 's/\$IMAGENAME/'"$IMAGE"'/' >Dockerfile
-    if [ "$( docker ps -a | grep $EL_CONTAINERNAME )" != "" ]; then
+    if [ "$( docker ps | grep $EL_CONTAINERNAME )" != "" ]; then
+      echo "Stopping running container..."
       docker stop $EL_CONTAINERNAME
+    fi
+    if [ "$( docker ps -a | grep $EL_CONTAINERNAME )" != "" ]; then
+      echo "Removing previously created container..."
       docker rm $EL_CONTAINERNAME
     fi
     if [ "$( docker images | grep $EL_CONTAINERNAME )" != "" ]; then
+      echo "Removing previously built image..."
       docker rmi $EL_CONTAINERNAME
     fi
+    echo "Building image..."
     docker build -t $EL_CONTAINERNAME .
     RESTART=""
     if [ "$EL_AUTORESTART" = "yes" ]; then
       RESTART="--restart yes"
     fi
-    docker create $RESTART -h stream --name $EL_CONTAINERNAME $EL_CONTAINERNAME
-    # docker create $RESTART -h stream -i --name $EL_CONTAINERNAME $EL_CONTAINERNAME
+    echo "Creating container..."
+    docker create $RESTART -h stream -it --name $EL_CONTAINERNAME -v $( pwd )/../$EL_DATA:/var/www -v $( pwd )/../$EL_LOG:/var/log -p 80:80/tcp -p 443:443/tcp -p 1935:1935/tcp $EL_CONTAINERNAME
+    echo "Starting container..."
     docker start $EL_CONTAINERNAME
+    if [ "$( docker ps | grep $EL_CONTAINERNAME )" = "" ]; then
+      echo "!!! Something went wrong with the container creation. See the errors above!"
+      exit 4
+    fi
+    echo "Container \"$EL_CONTAINERNAME\" was started, services are running inside."
+    echo "You can find the data under $EL_DEPLOY/$EL_DATA and the logs under $EL_DEPLOY/$EL_LOG"
+    echo "To control, use:"
+    echo "docker stop $EL_CONTAINERNAME # to stop the container"
+    echo "docker start $EL_CONTAINERNAME # to start it again"
+    echo "docker attach $EL_CONTAINERNAME # to log in for inspection"
+    echo "To detach without termination, use Ctrl+P+Q. Using the exit command will shut the container down."
     ;;
   "host")
     ./install.sh
+    echo "Services were installed and started. You can control them with your distribution's service manager (See systemd!)."
     ;;
 esac
 cd ../..
+echo "TCP ports HTTP(80), HTTPS(443) and RTMP(1935) are used by the service and are now open. Open them on your firewall if you use any to let the outside world connect."
+echo "Have fun!"
+exit 0
