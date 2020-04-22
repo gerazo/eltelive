a9988fba10194031ad339a3f2c8645ad56752773
Zoltan Gera <gerazo@caesar.elte.hu>
2020-04-22 13:23:30 +0200

    Setting up helpers

--- sh/env-conf.sh
+++ sh/env-conf.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 echo "Setting up helper services and watchdogs..."
+
+case "$EL_OS" in
+  "alpine")
+    cp stream-tokens /etc/periodic/daily/
+    ;;
+  "debian")
+    cp stream-tokens /etc/cron.daily/
+    ;;
+esac
