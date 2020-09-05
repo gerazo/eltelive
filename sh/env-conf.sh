4f7e9d69b16815576a3efae9b2a028e5a95674d5
Zoltan Gera <gerazo@caesar.elte.hu>
2020-09-05 18:34:35 +0200

    Hotfix: Add missing executable right for token cleansing

--- sh/env-conf.sh
+++ sh/env-conf.sh
@@ -5,8 +5,10 @@ echo "Setting up helper services and watchdogs..."
 case "$EL_OS" in
   "alpine")
     cp stream-tokens /etc/periodic/daily/
+    chmod +x /etc/periodic/daily/stream-tokens
     ;;
   "debian")
     cp stream-tokens /etc/cron.daily/
+    chmod +x /etc/cron.daily/stream-tokens
     ;;
 esac
