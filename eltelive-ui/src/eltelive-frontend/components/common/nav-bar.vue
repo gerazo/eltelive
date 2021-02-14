ce6e4fba3019962b3d1f8a651bbdda2bcb01568b
Nadeen.HUSSEIN@ibm.com <nadeen.husseinibm.com@Nadeens-MacBook-Air.local>
2021-02-14 17:03:51 +0100

    fix: front-back ends locations

--- eltelive-ui/src/eltelive-frontend/components/common/nav-bar.vue
+++ eltelive-ui/src/eltelive-frontend/components/common/nav-bar.vue
@@ -2,25 +2,23 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <img class= "logo" src="img/logo.png" alt="logo">
-  
-     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  </div>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
       <ul class="navbar-nav my-2 my-sm-0">
-      <li class="navbar-brand">
-        <router-link to="/home" class="nav-link">Home</router-link>
-      </li>
-      <li class="navbar-brand">
-         <router-link to="/about" class="nav-link">About</router-link>
-      </li>
-      <li class="navbar-brand">
-         <router-link to="/contact" class="nav-link">Contact</router-link>
-      </li>
-      <li class="navbar-brand">
-         <router-link to="/login" class="nav-link">Login</router-link>
-      </li>
-    </ul>
-</nav>
-
+        <li class="navbar-brand">
+          <router-link to="/home" class="nav-link">Home</router-link>
+        </li>
+        <li class="navbar-brand">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="navbar-brand">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
+        <li class="navbar-brand">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+      </ul>
+   </nav>
   </div>
 </template>
 
