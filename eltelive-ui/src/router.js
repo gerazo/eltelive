4cb5f3b0c6b5e774e032bd727c6e665eb58c467e
Nadeen.HUSSEIN@ibm.com <nadeen.husseinibm.com@Nadeens-MacBook-Air.local>
2021-02-17 14:39:31 +0100

    fix: components frontend paths

--- eltelive-ui/src/router.js
+++ eltelive-ui/src/router.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HomePageComponent from '@/components/home-page';
-import AboutComponent from '@/components/about-us';
-import ContactComponent from '@/components/contact-us';
-import SignUpComponent from '@/components/user/sign-up';
-import GuestComponent from '@/components/user/login-as-guest';
-import ProfileComponent from '@/components/user/profile';
-import LoginComponent from '@/components/user/login';
-import LegalDisclaimer from '@/components/legal-disclaimer';
-import ActiveStreamsComponent from '@/components/stream/active-stream'
-import CreateStreamComponent from '@/components/stream/create-stream';
-import NotFoundComponent from '@/components/not-found-page';
+
+import HomePageComponent from '@/eltelive-frontend/components/home-page';
+import AboutComponent from '@//eltelive-frontend/components/about-us';
+import ContactComponent from '@/eltelive-frontend/components/contact-us';
+import SignUpComponent from '@/eltelive-frontend/components/user/sign-up';
+import GuestComponent from '@/eltelive-frontend/components/user/login-as-guest';
+import ProfileComponent from '@/eltelive-frontend/components/user/profile';
+import LoginComponent from '@/eltelive-frontend/components/user/login';
+import LegalDisclaimer from '@/eltelive-frontend/components/legal-disclaimer';
+import ActiveStreamsComponent from '@/eltelive-frontend/components/stream/active-stream'
+import CreateStreamComponent from '@/eltelive-frontend/components/stream/create-stream';
+import NotFoundComponent from '@/eltelive-frontend/components/not-found-page';
 
 Vue.use(Router)
 
