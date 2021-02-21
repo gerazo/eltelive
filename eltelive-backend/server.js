c790563f2b1eee5d27cd96707353b1ab1d8f2a77
Shady Taodharos <shadyessam336@gmail.com>
2021-02-21 17:19:04 +0200

    The backend side with a temporary static part for testing

---
+++ eltelive-backend/server.js
@@ -0,0 +1,132 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+mongoose.connect('mongodb://localhost:27017/db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+})
+
+const app = express()
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
+
+app.post('/api/change-password', async (req, res) => {
+	console.log
+
+	const { newPassword: newPlainTextPassword, token } = req.body
+
+	if (!newPlainTextPassword || typeof newPlainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (newPlainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 5 characters'
+		})
+	}
+
+	try {
+		const user = jwt.verify(token, JWT_SECRET)
+
+		const _id = user.id
+
+		const password = await bcrypt.hash(newPlainTextPassword, 10)
+
+		await User.updateOne(
+			{ _id },
+			{
+				$set: { password }
+			}
+		)
+		res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: ';))' })
+	}
+})
+
+app.post('/api/login', async (req, res) => {
+	const { givenName, familyName, email, password } = req.body
+	const user = await User.findOne({ email }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				email: user.email
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', token: token, username: `${user.givenName} ${user.familyName}`})
+	}
+
+	res.json({ status: 'error', error: 'Invalid email/password' })
+})
+
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
+	const { givenName, familyName, email, password: plainTextPassword } = req.body
+
+	if (!givenName || typeof givenName !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid Given Name' })
+	}
+
+	if (!familyName || typeof familyName !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid Family Name' })
+	}
+
+	if (!email || typeof email !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid Email Address' })
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 5 characters'
+		})
+	}
+
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await User.create({
+			givenName,
+			familyName,
+			email,
+			password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
+
+	res.json({ status: 'ok' })
+})
+
+app.listen(4000, () => {
+	console.log('Server up at 4000')
+})
