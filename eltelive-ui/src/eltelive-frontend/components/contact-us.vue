45fd5fb5bac7ee0040989c7a1861d30a53963289
Nadeen.HUSSEIN@ibm.com <nadeen.husseinibm.com@Nadeens-MacBook-Air.local>
2021-03-16 23:31:16 +0100

    fix: css fix and add tsconfig

--- eltelive-ui/src/eltelive-frontend/components/contact-us.vue
+++ eltelive-ui/src/eltelive-frontend/components/contact-us.vue
@@ -1,38 +1,36 @@
 <template>
     <div class="contact">
-        <div class="container" >
-             <h1 class="title text-left">CONTACT US</h1>
-             <div class="row">
-             <div class="side-text-left  border-right">
-                    <div class="side-text-left-p"> 
-                        <h1>LET'S TALK</h1>
-                        <p>WANT TO SPEAK TO US? WE ARE HAPPY TO BE IN CONTACT!</p>
-                    </div>
-            </div>
-            <div class="right-side">
-                <div class="col">
-                      <div class="right-side-p">
-                          <div>
-                              <img src="/img/location-graphic.svg" alt="">
-                              Location : ELTE Faculty of Informatics, 1117 Budapest, Pázmány P. sny. 1/C
-                            </div>
-                          <div>
-                                <img src="/img/phone-graphic.svg" alt="">
-                                TelePhone: ELTELive Team
-                            </div>
-                          <div>
-                                <img class="email-icon" src="/img/email-graphic.svg" alt="">
-                                Email : SomeEmail@someemail.com
-                            </div>
-                    </div>  
-                <div class="contact-graphic col">
-                     <img src="/img/contact-graphic.svg" alt="">
+        <h1 class="title text-left">CONTACT US</h1>
+      <div class="content d-flex">
+        <div class="side-text-left border-right">
+          <div class="side-text-left-p"> 
+            <h1>LET'S TALK</h1>
+            <p>WANT TO SPEAK TO US? WE ARE HAPPY TO BE IN CONTACT!</p>
+          </div>
+          </div>
+            <div class="right-side pl-3">
+              <div>
+                <div class="right-side-p">
+                  <div>
+                    <img src="/img/location-graphic.svg" alt="">
+                      Location : ELTE Faculty of Informatics, 1117 Budapest, Pázmány P. sny. 1/C
+                  </div>
+                  <div>
+                    <img src="/img/phone-graphic.svg" alt="">
+                      TelePhone: ELTELive Team
+                  </div>
+                  <div>
+                    <img class="email-icon" src="/img/email-graphic.svg" alt="">
+                      Email : SomeEmail@someemail.com
+                  </div>
+                </div>  
+                <div class="contact-graphic">
+                    <img class="image" src="/img/contact-graphic.svg" alt=""> 
                 </div>
-                </div>
-             </div>       
-            </div>
-         </div>
-     </div>
+              </div>
+            </div>  
+      </div>     
+    </div>
 </template>
 
 <script>
@@ -43,15 +41,14 @@
 
 <style lang="scss">
 $color:#438bac;
+$mid: 540px;
 .contact{
-    max-width: 70%;
-    max-height: 70%;
-    margin : 50px auto;
+    margin : 3rem 15rem;
     background-color:#f1f3f5;
     box-shadow: 0 10px 30px 0px rgba(155, 207, 241, 0.1);
     padding: 30px;
+    min-width: 400px;
 }
-
 .title{
     color: $color;
 }
@@ -59,25 +56,27 @@ $color:#438bac;
     width:20px;
     height:20px;
 }
-
+.contact-graphic{
+margin: 10px;
+height:auto;
+overflow:auto;
+}
 .side-text-left{
-    padding-top:30%;
-    padding-bottom:5%;
-    width: 40%;
-    text-align: left;
+    padding-top:15rem;
+    width: 23rem;
 }
 .side-text-left-p{
     color: $color;
-    font-size: 30px;
-}
-.right-side{
-    width: 50%;
-    text-align: left;
-    padding-top:10%;
+    font-size: 1.8rem;
 }
 .right-side-p{
     color: rgb(128, 125, 125);
-    font-size: 15px;
+    font-size: 1rem;
     font-weight: bold;
 }
+.image{
+  height: auto;
+  max-width: 500px;
+}
+
 </style>
