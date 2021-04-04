dd7191b63d8860807df56bd1cfb0ebc05d931c6b
Nadeen.HUSSEIN@ibm.com <nadeen.husseinibm.com@Nadeens-MacBook-Air.local>
2021-04-04 12:56:38 +0200

    fix: function add

--- eltelive-ui/src/eltelive-frontend/components/user/login-as-guest.vue
+++ eltelive-ui/src/eltelive-frontend/components/user/login-as-guest.vue
@@ -58,49 +58,51 @@
 
 <script>
 export default {
-  name: "login-as-guest"
-};
-
-function registerStreamKey(type) {
-  var streamKey = document.getElementById("streamkey").value;
-  if(!streamKey)
-    return;
-  if (type == "1") {
-    // RTMP
-    var streamLink = "rtmp://$DOMAINNAME/live/" + streamKey;
-    window.open(streamLink, "_blank");
-  } else if (type == "2") {
-    //MPEG-DASH
-    var streamLink =
-      "https://$DOMAINNAME/livedash/" + streamKey + ".mpd";
-    var player = videojs('stream-player');
-    player.src({src: streamLink, type: 'application/dash+xml'});
-    player.play();
-  } else if (type == "3") {
-    //HLS
-    var streamLink =
-      "https://$DOMAINNAME/livehls/" + streamKey + ".m3u8";
-    var player = videojs('stream-player');
-    player.src({src: streamLink, type: 'application/x-mpegURL'});
-    player.play();
-  } else if (type == "4") {
-    //Recorded videos
-    var streamLink =
-      "https://$DOMAINNAME/videos/" + streamKey;
-    window.open(streamLink, "_blank");
-  } else if (type == "5") {
-    //RTMP low quality
-    var streamLink = "rtmp://$DOMAINNAME/livelq/" + streamKey;
-    window.open(streamLink, "_blank");
-  } else if (type == "6") {
-    //HLS low quality
-    var streamLink =
-      "https://$DOMAINNAME/livehlslq/" + streamKey + ".m3u8";
-    var player = videojs('stream-player');
-    player.src({src: streamLink, type: 'application/x-mpegURL'});
-    player.play();
+  name: "login-as-guest",
+  methods: {
+      registerStreamKey(type) {
+      var streamKey = document.getElementById("streamkey").value;
+      if(!streamKey)
+        return;
+      if (type == "1") {
+        // RTMP
+        var streamLink = "rtmp://$DOMAINNAME/live/" + streamKey;
+        window.open(streamLink, "_blank");
+      } else if (type == "2") {
+        //MPEG-DASH
+        var streamLink =
+          "https://$DOMAINNAME/livedash/" + streamKey + ".mpd";
+        var player = videojs('stream-player');
+        player.src({src: streamLink, type: 'application/dash+xml'});
+        player.play();
+      } else if (type == "3") {
+        //HLS
+        var streamLink =
+          "https://$DOMAINNAME/livehls/" + streamKey + ".m3u8";
+        var player = videojs('stream-player');
+        player.src({src: streamLink, type: 'application/x-mpegURL'});
+        player.play();
+      } else if (type == "4") {
+        //Recorded videos
+        var streamLink =
+          "https://$DOMAINNAME/videos/" + streamKey;
+        window.open(streamLink, "_blank");
+      } else if (type == "5") {
+        //RTMP low quality
+        var streamLink = "rtmp://$DOMAINNAME/livelq/" + streamKey;
+        window.open(streamLink, "_blank");
+      } else if (type == "6") {
+        //HLS low quality
+        var streamLink =
+          "https://$DOMAINNAME/livehlslq/" + streamKey + ".m3u8";
+        var player = videojs('stream-player');
+        player.src({src: streamLink, type: 'application/x-mpegURL'});
+        player.play();
+      }
+    }
   }
-}
+};
+	
 </script>
 
 <style lang="scss">
