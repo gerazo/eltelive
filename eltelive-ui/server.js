ede9926fec76e5d6deab8f84f4c0aaa57d66e09d
Shady Taodharos <shadyessam336@gmail.com>
2021-04-07 00:31:45 +0200

    Add Unit Tests for APIs + Add status codes for HTTP responses

--- eltelive-ui/server.js
+++ eltelive-ui/server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const User = require('./model/user')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
@@ -12,23 +12,27 @@ mongoose.connect('mongodb://localhost:27017/db', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true
-})
+});
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.post('/api/change-password', async (req, res) => {
 	const { newPassword: newPlainTextPassword, token } = req.body
 
+	if(!token || typeof token !== 'string') {
+		return res.status(400).json({ status: 'error', error: 'Token not provided' })
+	}
+
 	if (!newPlainTextPassword || typeof newPlainTextPassword !== 'string') {
-		return res.json({ status: 'error', error: 'Invalid password' })
+		return res.status(401).json({ status: 'error', error: 'Invalid password' })
 	}
 
 	if (newPlainTextPassword.length < 5) {
-		return res.json({
+		return res.status(403).json({
 			status: 'error',
-			error: 'Password too small. Should be atleast 5 characters'
+			error: 'Password too small. It should be at least 5 characters'
 		})
 	}
 
@@ -45,18 +49,19 @@ app.post('/api/change-password', async (req, res) => {
 				$set: { password }
 			}
 		)
-		res.json({ status: 'ok' })
+		res.status(200).json({ status: 'ok' })
 	} catch (error) {
 		console.log(error)
-		res.json({ status: 'error', error: ';))' })
+		res.status(400).json({ status: 'error', error: 'Invalid token' })
 	}
-})
+});
 
 app.get('/api/user', async (req, res) => {
 	const token = req.headers.token
 
 	jwt.verify(token, JWT_SECRET, async (err, decoded) => {
 		if (err) return res.status(401).json({
+			status: 'error',
 			title: 'Unauthorized'
 		})
 
@@ -64,10 +69,11 @@ app.get('/api/user', async (req, res) => {
 		const user = await User.findOne({ _id: decoded.id }).lean()
 
 		if (!user) {
-			return res.json({ status: 'error', error: 'User does not exist' })
+			return res.status(404).json({ status: 'error', error: 'User does not exist' })
 		}
 		return res.status(200).json({
-			title: 'user grabbed',
+			status: 'ok',
+			title: 'User details are retrived successfully',
 			user: {
 				givenName: user.givenName,
 				familyName: user.familyName,
@@ -82,21 +88,21 @@ app.post('/api/login', async (req, res) => {
 	const { email, password: plainTextPassword } = req.body
 
 	if (!email || typeof email !== 'string') {
-		return res.json({ status: 'error', error: 'Missing Email Address' })
+		return res.status(400).json({ status: 'error', error: 'Missing Email Address' })
 	}
 
 	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-		return res.json({ status: 'error', error: 'Missing password' })
+		return res.status(400).json({ status: 'error', error: 'Missing password' })
 	}
 
 	// regular expression for matching email addresses
 	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if ( !re.test(email.toLowerCase()) ) {
-		return res.json({ status: 'error', error: 'Email Address is invalid' })
+		return res.status(400).json({ status: 'error', error: 'Email Address is invalid' })
 	}
 
 	if (plainTextPassword.length < 5) {
-		return res.json({
+		return res.status(403).json({
 			status: 'error',
 			error: 'Password is too small. It should be at least 5 characters'
 		})
@@ -105,7 +111,7 @@ app.post('/api/login', async (req, res) => {
 	const user = await User.findOne({ email }).lean()
 
 	if (!user) {
-		return res.json({ status: 'error', error: 'Invalid email/password' })
+		return res.status(401).json({ status: 'error', error: 'Invalid email/password' })
 	}
 
 	if (await bcrypt.compare(plainTextPassword, user.password)) {
@@ -118,40 +124,43 @@ app.post('/api/login', async (req, res) => {
 			},
 			JWT_SECRET
 		)
+		console.log('User logged in successfully!')
+		console.log('Token: ', token)
+		console.log('Username: ', `${user.givenName} ${user.familyName}`)
 
-		return res.json({ status: 'ok', token: token, username: `${user.givenName} ${user.familyName}`})
+		return res.status(200).json({ status: 'ok', token: token, username: `${user.givenName} ${user.familyName}`})
 	}
 
-	res.json({ status: 'error', error: 'Invalid email/password' })
+	res.status(401).json({ status: 'error', error: 'Invalid email/password' })
 })
 
 app.post('/api/register', async (req, res) => {
 	const { givenName, familyName, email, password: plainTextPassword } = req.body
 
 	if (!givenName || typeof givenName !== 'string') {
-		return res.json({ status: 'error', error: 'Missing Given Name' })
+		return res.status(400).json({ status: 'error', error: 'Missing Given Name' })
 	}
 
 	if (!familyName || typeof familyName !== 'string') {
-		return res.json({ status: 'error', error: 'Missing Family Name' })
+		return res.stats(400).json({ status: 'error', error: 'Missing Family Name' })
 	}
 
 	if (!email || typeof email !== 'string') {
-		return res.json({ status: 'error', error: 'Missing Email Address' })
+		return res.status(400).json({ status: 'error', error: 'Missing Email Address' })
 	}
 
 	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-		return res.json({ status: 'error', error: 'Missing password' })
+		return res.status(400).json({ status: 'error', error: 'Missing password' })
 	}
 
 	// regular expression for matching email addresses
 	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if ( !re.test(email.toLowerCase()) ) {
-		return res.json({ status: 'error', error: 'Email Address is invalid' })
+		return res.status(400).json({ status: 'error', error: 'Email Address is invalid' })
 	}
 
 	if (plainTextPassword.length < 5) {
-		return res.json({
+		return res.status(403).json({
 			status: 'error',
 			error: 'Password is too small. It should be at least 5 characters'
 		})
@@ -170,12 +179,12 @@ app.post('/api/register', async (req, res) => {
 	} catch (err) {
 		if (err.code === 11000) {
 			// duplicate key
-			return res.json({ status: 'error', error: 'Email already in use' })
+			return res.status(409).json({ status: 'error', error: 'Email already in use' })
 		}
 		throw err
 	}
 
-	res.json({ status: 'ok' })
+	res.status(200).json({ status: 'ok' })
 })
 
 const port = process.env.PORT || 4000;
@@ -184,3 +193,5 @@ app.listen(port, (err) => {
   if (err) return console.log(err);
   console.log('server running on port ' + port);
 })
+
+module.exports = app
