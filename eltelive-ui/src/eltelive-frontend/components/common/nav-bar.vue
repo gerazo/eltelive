3349c067c6b1378985b4692c2658fcf9d07eee94
NadeenHussein <nadeenahmed_a@yahoo.com>
2021-04-28 05:26:58 +0300

    feat: delete key

--- eltelive-ui/src/eltelive-frontend/components/common/nav-bar.vue
+++ eltelive-ui/src/eltelive-frontend/components/common/nav-bar.vue
@@ -4,7 +4,21 @@
     <img class= "logo" src="img/logo.png" alt="logo">
     
     <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
-      <ul class="navbar-nav my-2 my-sm-0">
+      <ul class="navbar-nav my-2 my-sm-0" ref="loggedIn" v-if="isloggedin">
+        <li class="navbar-brand">
+          <router-link to="/home" class="nav-link">Home</router-link>
+        </li>
+        <li class="navbar-brand">
+          <router-link to="/about" class="nav-link">About Us</router-link>
+        </li>
+        <li class="navbar-brand">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
+        <li class="navbar-brand">
+          <router-link to="/login" class="nav-link">Log Out</router-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav my-2 my-sm-0" ref="notLoggedIn" v-else>
         <li class="navbar-brand">
           <router-link to="/home" class="nav-link">Home</router-link>
         </li>
@@ -28,6 +42,30 @@
 <script>
 export default {
   name: "NavBar",
+  data: function() {
+        return {
+            isloggedin: true
+        }
+    },
+  mounted() {
+    const isLoggedInTrue = this.$refs['loggedIn'];
+    isLoggedInTrue.addEventListener('keyup',isLoggedIn);
+
+    const isNotLoggedIn = this.$refs['notLoggedIn'];
+    isNotLoggedIn.addEventListener("keyup",isNotIn);
+    
+    function isLoggedIn(){
+        if(localStorage.getItem('token') !== undefined){
+          this.isloggedin = false;
+        }
+        console.log("LoggedIn? is " + this.isloggedin)
+      }
+    function isNotIn () {
+      if(localStorage.getItem('token') === undefined){
+          this.isloggedin = true;
+        }
+    }
+    }
 };
 </script>
 <style lang="scss">
--- eltelive-ui/src/eltelive-frontend/components/stream/active-streams.vue
+++ eltelive-ui/src/eltelive-frontend/components/stream/active-streams.vue
@@ -4,23 +4,23 @@
     <div class="text-center pt-5 pb-5">
       <h1>Active Streams</h1>
     </div>
-    <div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center pb-5">
       <div class="jumbotron w-50 p-3 mb-2">
       <p class="pt-3 pr-1 pl-3 lead">
-        The <span class="h4">STREAM KEY</span> that is currently displayed is the key that you as a 
+        The <span class="h4 font-weight-bold">STREAM KEY</span> that is currently displayed is the key that you as a 
         streamer should copy to OBS. This key should be shared with your audience for them 
         to able to access your stream.
       </p>
       </div>
     </div>
-    <div class="d-flex justify-content-center w-50 pt-5 pl-5">
+    <div class="d-flex justify-content-center pt-5 pl-5">
       <div>
-        <h6>Stream Key:</h6>
+        <h4 class="pt-2 pr-4 font-weight-bold">Stream Key:</h4>
       </div>
       <form id="keyGeneration" ref="keyGeneration">
         <div class="d-flex">
           <div class="streamKeyField border">
-            <p placeholder="Stream Key" class="pt-2" id="key_textfield"></p>
+            <p placeholder="Stream Key" class="pt-2 font-weight-bold" id="key_textfield"></p>
           </div>
           <div class="input-group-append">
             <input class="btn btn-primary" ref="keyGenerationStream" type="button" value="Generate Key"/>
@@ -36,9 +36,6 @@
 <script>
 export default {
   name: "active-streams",
-  methods: {
-
-  },
   mounted() {
     const generateButton = this.$refs['keyGenerationStream']
     generateButton.addEventListener('click',generateStreamKey)
@@ -93,5 +90,10 @@ export default {
     min-width: 7rem;
     padding-left: 1rem;
     padding-right: 2rem;
+
+    p{
+      font-size:1.3rem;
+    }
   }
+
 </style>
