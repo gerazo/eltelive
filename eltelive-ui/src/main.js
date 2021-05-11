23d0bef4422f41c22a8c733954cacd765c048357
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-05-12 01:05:13 +0200

    fix: remove notification library

--- eltelive-ui/src/main.js
+++ eltelive-ui/src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router';
 import 'bootstrap';
 import './assets/app.scss';
-import Notifications from 'vue-notification';
+
 Vue.config.productionTip = false;
 
 Vue.component('NavBar',require('./components/common/nav-bar.vue').default);
 Vue.component('Footer',require('./components/common/footer.vue').default);
-Vue.use(Notifications);
+
 
 new Vue({
   router,
