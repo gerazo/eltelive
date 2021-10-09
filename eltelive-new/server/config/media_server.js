4c0d5045e27a312ca599b9847055d6f760ccef47
=arbab ali <d1cpld@inf.elte.hu>
2021-10-09 16:09:08 +0200

    just saving stuff

--- eltelive-new/server/config/media_server.js
+++ eltelive-new/server/config/media_server.js
@@ -5,7 +5,38 @@ nms = new NodeMediaServer(config);
 const nms_context = require('node-media-server/src/node_core_ctx.js')
 let cache_list = []
 // Check if the stream key used to watch the stream exists in the database or not
+const resolution_standard_video_map ={
+    '240p':{'30':1000,
+                '60':1500,},
+    '360p':{'30':1000,
+        '60':1500,},
+    '480p':{'30':2500,
+        '60':4000,},
+    '720p':{'30':5000,
+        '60':7500,},
+    '1080p':{'30':8000,
+        '60':12000,},
+    '2k':{'30':16000,
+        '60':24000,},
+    '4k':{'30':35000,
+        '60':53000,},
+}
 
+const get_video_resolution= (width,height)=>{
+    if (width<480 & height<360){
+        return '240p'
+    }else if(width<858 & height<480){
+        return '360p'
+    }else if (width<1280 & height<729){
+        return '480p'
+    }else if (width<1920 & height<1080){
+        return '720p'
+    }else if (width<3680 & height<2160){
+        return '1080p'
+    }else{
+        return '4k'
+    }
+}
 nms.on('prePublish', async (id, StreamPath, args) => {
     let stream_key = getStreamKeyFromStreamPath(StreamPath);
     console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
@@ -39,6 +70,15 @@ nms.on('prePublish', async (id, StreamPath, args) => {
         }
         console.log('bitrate ',bitrate)
         console.log('fps ',fps)
+        console.log('video code ',session.videoCodec)
+        const pixel=get_video_resolution(session.videoWidth,session.videoHeight)
+        console.log('video codeName ',session.videoCodecName )
+        console.log('video ProfileName ',session.videoProfileName)
+        console.log('video videoWidth ',session.videoWidth)
+        console.log('video videoHeight ',session.videoHeight)
+
+        console.log('video pixels ',pixel)
+        console.log('video standard rate',resolution_standard_video_map[pixel][fps.toString()])
 
 
     },2000)
