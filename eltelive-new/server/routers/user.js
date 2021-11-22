2d1bdd60eb31e8c7ed3de5c53468634e197157fb
=arbab ali <d1cpld@inf.elte.hu>
2021-11-22 21:11:00 +0100

    refactor: updated controller

--- eltelive-new/server/routers/user.js
+++ eltelive-new/server/routers/user.js
@@ -1,34 +1,32 @@
 const express = require('express');
-
 const auth  = require('../middleware/auth')
-const {getUsers,getUser,getStreamKey, registerUser,loginUser,deleteUser,changePassword,generateKey,deleteKey} = require('../controllers/user')
-
+const controller = require('../controllers/user')
 const router = new express.Router()
 
 
 
 
 // GET functions
-router.get('/api/get_user', auth, getUser)
-router.get('/api/get_users', auth ,getUsers)
-router.get('/api/get_key', auth, getStreamKey)
+router.get('/api/get_user', auth, controller.getUser)
+router.get('/api/get_users', auth ,controller.getUsers)
+router.get('/api/get_key', auth, controller.getStreamKey)
 
 // POST functions
-router.post('/api/register',registerUser)
-router.post('/api/login', loginUser)
+router.post('/api/register',controller.registerUser)
+router.post('/api/login', controller.loginUser)
 
 
 // DELETE functions
-router.delete('/api/delete_user',auth,deleteUser)
+router.delete('/api/delete_user',auth,controller.deleteUser)
 
 // PATCH functions
-router.patch('/api/change_password', auth,changePassword)
+router.patch('/api/change_password', auth,controller.changePassword)
 
 // PUT functions
-router.put('/api/generate_key', auth , generateKey)
+router.put('/api/generate_key', auth ,controller.generateKey)
 
 
 
-router.delete('/api/delete_key',auth, deleteKey)
+router.delete('/api/delete_key',auth, controller.deleteKey)
 
 module.exports = router
